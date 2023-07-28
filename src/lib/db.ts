a37d7bc46005ba64b321a83ffed3aa821267216d
mohamed-abdulla <abdabdulla78@gmail.com>
2023-07-28 08:30:37 +0530

    env changed

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,6 +2,8 @@ import { Redis } from "@upstash/redis";
 import "dotenv/config";
 
 export const db = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL!,
-  token: process.env.UPSTASH_REDIS_REST_TOKEN!,
+  url: 'https://destined-parakeet-38275.upstash.io',
+  token: "AZWDASQgNjlkZjVhMWUtYmU3Mi00ZGQ4LTg5MjEtZGIxNzEzZjEzMDAyODAxN2FhOWMwNTA4NGNmY2IyMjY1MWRkNGI2ZTBiM2U=",
+  // url: process.env.UPSTASH_REDIS_REST_URL!,
+  // token: process.env.UPSTASH_REDIS_REST_TOKEN!,
 });
